3065d3d888e11d445bb3c3f57317357646c7adf1
kaleb <39206468+khicken@users.noreply.github.com>
2020-10-21 19:35:54 -0700

    updated vscode build tasks, added cvui lib, inplemented better window closure

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,51 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#define CVUI_IMPLEMENTATION
+#include <src/cvui.h> // import cv gui
 
-using namespace cv;
-using namespace std;
+std::string WINDOW_NAME = "Camera Thing";
+unsigned int cameraDevice = 0;
 
 int main() {
-    cout << "help" << endl;
-    // Open the default video camera
-    VideoCapture cap(0);
+    cv::VideoCapture cap(cameraDevice); // open default video camera
 
-    // if not success, exit program
-    if (cap.isOpened() == false) {
-        cout << "Cannot open the video camera" << endl;
-        cin.get(); //wait for any key press
+    if(cap.isOpened() == false) {
+        std::cout << "cannot open video camera!" << std::endl;
+        std::cin.get(); //wait for any key press
         return -1;
     }
 
-    double dWidth = cap.get(CAP_PROP_FRAME_WIDTH);   //get the width of frames of the video
-    double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+    double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);   //get the width of frames of the video
+    double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
-    cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
+    std::cout << "res of video capture: " << dWidth << " x " << dHeight << std::endl;
 
-    string window_name = "My Camera Feed";
-    namedWindow(window_name); // create a window called "My Camera Feed"
+    cv::Mat frame = cv::Mat(1000, 1000, CV_8UC3);
+    cv::namedWindow(WINDOW_NAME); // create a window called "My Camera Feed"
+    cvui::init(WINDOW_NAME);
 
+    // window loop until exited/esc key pressed
     while(true) {
-        Mat frame;
-        bool bSuccess = cap.read(frame); // read a new frame from video
+        frame = cv::Scalar(255, 0, 0); // background
+        bool bSuccess = cap.read(frame); // read a new frame from capture device
 
-        // Breaking the while loop if the frames cannot be captured
-        if (bSuccess == false) {
-            cout << "Video camera is disconnected" << endl;
-            cin.get(); // Wait for any key press
+        if(bSuccess == false) { // if capture device dead
+            std::cout << "cannot locate video capture device" << std::endl;
+            std::cin.get(); // wait till user p*resses key in console
             break;
         }
+
+        // if (cvui::button(frame, 100, 100, "random button"))
+        //     std::cout << "yooo" << std::endl;
+        // cvui::printf(frame, 100, 100, 0.5, 0xff0000, "random text");
 
         // show the frame in the created window
-        imshow(window_name, frame);
+        cvui::update();
 
-        if(waitKey(10) == 27) {
-            cout << "Esc key is pressed by user. Stoppig the video" << endl;
+        cv::imshow(WINDOW_NAME, frame);
+
+        if(cv::waitKey(1) == 27 || cv::getWindowProperty(WINDOW_NAME, cv::WND_PROP_VISIBLE) < 1)
             break;
-        }
     }
 
     return 0;
